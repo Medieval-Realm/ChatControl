a92c3059190e367ce24bf6c9dd9b258347e963a6
Sylensky <admin@medieval-realm.net>
2025-10-09 23:33:41 +0000

    fix PartiesHook integration

--- chatcontrol-bungeecord/src/main/java/org/mineacademy/chatcontrol/bungee/hook/PartiesHook.java
+++ chatcontrol-bungeecord/src/main/java/org/mineacademy/chatcontrol/bungee/hook/PartiesHook.java
@@ -2,8 +2,6 @@ package org.mineacademy.chatcontrol.bungee.hook;
 
 import java.util.UUID;
 
-import javax.annotation.Nullable;
-
 import com.alessiodp.parties.api.Parties;
 import com.alessiodp.parties.api.interfaces.PartiesAPI;
 import com.alessiodp.parties.api.interfaces.PartyPlayer;
@@ -13,22 +11,22 @@ import com.alessiodp.parties.api.interfaces.PartyPlayer;
  */
 public final class PartiesHook {
 
-	/**
-	 * Set the nickname inside Parties plugin from the given nick (coming from ChatControl)
-	 *
-	 * @param uuid
-	 * @param nick
-	 */
-	public static void setNickName(final UUID uuid, @Nullable final String nick) {
-		final PartiesAPI api = Parties.getApi();
-		final PartyPlayer player = api.getPartyPlayer(uuid);
+    /**
+     * Set the nickname inside Parties plugin from the given nick (coming from ChatControl)
+     *
+     * @param uuid
+     * @param nick
+     */
+    public static void setNickName(final UUID uuid, final String nick) {
+        final PartiesAPI api = Parties.getApi();
+        final PartyPlayer player = api.getPartyPlayer(uuid);
 
-		if (player != null) {
-			final String oldNick = player.getNickname();
+        if (player != null) {
+            final String oldNick = player.getNickname();
 
-			// Only set the nickname if the user has updated it on ChatControl
-			if ((oldNick != null && !oldNick.equals(nick)) || (nick != null && !nick.equals(oldNick)))
-				player.setNickname(nick);
-		}
-	}
+            // Only set the nickname if the user has updated it on ChatControl
+            if ((oldNick != null && !oldNick.equals(nick)) || (nick != null && !nick.equals(oldNick)))
+                player.setNickname(nick);
+        }
+    }
 }
